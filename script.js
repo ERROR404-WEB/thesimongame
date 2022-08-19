32c79c1f44501c8e6753e4c6b0fc96aa5806fe60
ARJUN KUMAR <102915876+ERROR404-WEB@users.noreply.github.com>
2022-08-19 23:04:18 +0530

    version update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var started = false;
 var level = 1, score = 0;
 var but = ['a', 'b', 'c', 'd'];
-var words=['green','red','red','blue'];
+var words = ['green', 'red', 'red', 'blue'];
 var randomm = "";
 var count = 0;
 var clickedstring = "";
@@ -14,70 +14,73 @@ $(document).keydown(function (event) {
         started = true;
     }
     else {
-        $(".textt2").removeClass("hide");
-        $('.texttt').text("Level " + level);
-        if(count==0) newelement();
-        count++;
+        begin();
     }
 
 });
+function begin() {
+    $(".textt2").removeClass("hide");
+    $('.texttt').text("Level " + level);
+    if (count == 0) newelement();
+    count++;
+}
 var usercount = 0;
 
 
-function newelement()
-{
-    var r=Math.random()*4;
-    r=Math.floor(r)+1;
-    playsound(but[r-1]);
-    randomm+=but[r-1];
+function newelement() {
+    var r = Math.random() * 4;
+    r = Math.floor(r) + 1;
+    playsound(but[r - 1]);
+    randomm += but[r - 1];
     index++;
     score++;
-    if(score%5==0)
-    {
+    if (score % 5 == 0) {
         level++;
         $('.texttt').text("Level " + level);
     }
-    $('.textt2').text(words[r-1]);
-    $('.textt2').css("text-shadow","0px 0px 10px"+words[r-1]);
-    $('.textt2').css("color",words[r-1]);
+    $('.textt2').text(words[r - 1]);
+    $('.textt2').css("text-shadow", "0px 0px 10px" + words[r - 1]);
+    $('.textt2').css("color", words[r - 1]);
 }
-var flag=0;
-$('.btn').on('click', function(){
+var flag = 0;
+$('.btn').on('click', function () {
 
-    var user=$(this).attr("id");
-    clickedstring+=user;
-    
-    playsound(user);
-    flag++;
-    if(clickedstring[flag-1]==randomm[flag-1])
-    {
-        if(flag==randomm.length)
-         check(clickedstring);
+    if (count== 0) {
+        begin();
+        count++;
     }
-    else
-    {
-       end();
+    else {
+        var user = $(this).attr("id");
+        clickedstring += user;
+
+        playsound(user);
+        flag++;
+        if (clickedstring[flag - 1] == randomm[flag - 1]) {
+            if (flag == randomm.length)
+                check(clickedstring);
+        }
+        else {
+            end();
+        }
     }
-    
- 
+
+
 });
 
-function check(ss)
-{
+function check(ss) {
     console.log(randomm);
     console.log(clickedstring);
-    if(randomm==clickedstring)
-    {
-        clickedstring="";
-        flag=0;
-        setTimeout(function(){
+    if (randomm == clickedstring) {
+        clickedstring = "";
+        flag = 0;
+        setTimeout(function () {
             newelement();
-        },500*level);
-       
+        }, 500 * level);
+
     }
     else {
         end();
-       
+
     }
 }
 
@@ -89,21 +92,21 @@ function check(ss)
 
 
 function end() {
-    
+
     $("body").removeClass("bg");
     $("body").addClass("red");
     $(".screen1").addClass("hide");
     $(".start-btn").removeClass("hide");
-    $(".texttt").text("Your Score :"+score);
+    $(".texttt").text("Your Score :" + score);
     $(".screen2").addClass("margin");
     $(".textt2").addClass("hide");
     playsound("wrong");
-    flag=0;
-    count=0;
-    level=1;
-    randomm="";
-    clickedstring="";
-    started=false;
+    flag = 0;
+    count = 0;
+    level = 1;
+    randomm = "";
+    clickedstring = "";
+    started = false;
 }
 function start() {
 
