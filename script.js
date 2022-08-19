e82e6f8ccc488ad498fd55e9427144e073b3d698
ARJUN KUMAR <102915876+ERROR404-WEB@users.noreply.github.com>
2022-08-19 16:09:31 +0530

    version update

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
+
+$(document).keydown(function(event){
+
+    start();
+
+
+});
+
 $('.btn').on("click", function(){
 
+    end();
 
-    alert("hello");
 });
+function end()
+{
+    $("body").removeClass("bg");
+    $("body").addClass("red");
+    $(".screen1").addClass("hide");
+    $(".start-btn").removeClass("hide");
+    $(".texttt").text("Your Score :");
+
+}
+function start()
+{
+    $("body").removeClass("red");
+    $("body").addClass("bg");
+    $(".screen1").removeClass("hide");
+    $(".start-btn").addClass("hide");
+    $('.texttt').text("Level : 1");
+    $(".screen2").removeClass("margin");
+}
+function help()
+{
+    var s="Its a game of memorizing things. 1)Memorize the buttons which blinks 2)In every blink you have to memorize the path and click the buttons in the same  order 3)Difficulty Increases as you pass Levels"
+    alert(s);
+}
